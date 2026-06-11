14a8ceea3c03e29a8c9d81dcb839f13e6496153e
isko6709 <isko6709@gmail.com>
2026-06-11 22:59:52 +0600

    Sdsd

--- mysite/admin/setup.py
+++ mysite/admin/setup.py
@@ -3,5 +3,5 @@ from mysite.database.db import engine
 
 
 def create_admin(app) -> Admin:
-    admin = Admin(app, engine, title="JohnAj Admin")
+    admin = Admin(app, engine, title="admin")
     return admin
